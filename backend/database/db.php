704872e73cc390c9a58fd5d6c53128bdd9cb807a
paulusel <paulusel@proton.me>
2025-04-25 12:47:21 +0300

    use class encapsulation to avoid namespace pollution

--- backend/database/db.php
+++ backend/database/db.php
@@ -1,17 +1,22 @@
 <?php
 
-$db_host = "localhost";
-$db_name = "nyetflix";
-$db_username = "nyetflix";
-$db_password = "";
+class Connection {
 
-try {
-    $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_username, $db_password);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch(PDOException $e) {
-    http_response_code(500);
-    // TODO: write server log
-    echo "Internal Server Error";
-    exit;
+private static $db_host = "localhost";
+private static $db_name = "nyetflix";
+private static $db_username = "nyetflix";
+private static $db_password = "";
+
+    function __construct() {
+        try {
+            $db = new PDO("mysql:host=$this->db_host;dbname=$this->db_name", $this->db_username, $this->db_password);
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch(PDOException $e) {
+            http_response_code(500);
+            // TODO: write server log
+            echo "Internal Server Error";
+            exit;
+        }
+    }
 }
