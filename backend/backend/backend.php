e9ab299b4d3e41ea2560e1857675bc1eaf4ff87c
paulusel <paulusel@proton.me>
2025-04-25 17:54:53 +0300

    wip: more developed backend code

--- backend/backend/backend.php
+++ backend/backend/backend.php
@@ -1,9 +1,11 @@
 <?php
 
+include "../logger.php";
+
 class BackendException extends Exception {}
 
-class Backend {
-
+class Backend
+{
     private static $db_host = "localhost";
     private static $db_name = "nyetflix";
     private static $db_username = "nyetflix";
@@ -16,16 +18,59 @@ class Backend {
             $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         catch(Exception $e) {
-            // TODO: write server log
-            throw new BackendException("Internal Server Error");
+            Logger::log($e->getTraceAsString());
+            throw new BackendException("internal server error", 500);
         }
     }
 
-    public function get_movie(string $movie_id) : array{
-        return []; // to be removed
+    public function getMovie(string $movie_id) : array{
+        try {
+            $db = $this->db;
+            $stmnt = $db->prepare("SELECT movie_id, title, description, thumbnail, video, FROM lists WHERE username = ?");
+            $stmnt->execute([$movie_id]);
+            $result = $stmnt->fetchAll();
+            if(empty($result)) {
+                throw new BackendException("movie not found", 404);
+            }
+            return $result;
+        }
+        catch(BackendException $e) {
+            throw $e;
+        }
+        catch(Exception $e) {
+            Logger::log($e->getTraceAsString());
+            throw new BackendException("internal server error", 500);
+        }
     }
 
-    public function get_lists(string $username) : array {
-        return []; // placeholder
+    public function getLists(string $username) : array {
+        try {
+            $db = $this->db;
+            $stmnt = $db->prepare("SELECT list_id, list_name FROM lists WHERE username = ?");
+            $stmnt->execute([$username]);
+            return $stmnt->fetchAll();
+        }
+        catch(BackendException $e) {
+            throw $e;
+        }
+        catch(PDOException $e) {
+            Logger::log($e->getTraceAsString());
+            throw new BackendException("internal server error", 500);
+        }
+    }
+
+    public function addUser(array $user) : void {
+        if(!isset($user->username, $user->password)) {
+            throw new BackendException("invalid user data", 400);
+        }
+
+        // process $user and add to database
+        try {
+
+        }
+        catch(Exception $e) {
+            Logger::log($e->getTraceAsString());
+            throw new BackendException("internal server error", 500);
+        }
     }
 }
