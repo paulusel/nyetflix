65e9c6828a773857fee2a16ec0d832fdd1a9c254
paulusel <paulusel@proton.me>
2025-05-07 14:12:14 +0300

    improve error response messages in movie api method

--- backend/api/movie.php
+++ backend/api/movie.php
@@ -9,12 +9,12 @@ try {
     $movie_id = json_decode(file_get_contents("php://input"), true);
 
     if(!$movie_id || !is_string($movie_id)) {
-        sendMessage("no movie_id specified in request", 400);
+        sendMessage("no movie id specified in request", 400);
         exit;
     }
 
     $movie = Backend::movie($movie_id);
-    sendJson($movie);
+    sendJson(["ok" => true, "movie" => $movie]);
 }
 catch(BackendException $e) {
     sendMessage($e->getMessage(), $e->getCode());
