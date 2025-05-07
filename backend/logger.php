33433c5a1dbd21c2e787632f6e3f63dd010ce3ac
paulusel <paulusel@proton.me>
2025-05-07 15:56:24 +0300

    use enum to indicate log levels to log messages

--- backend/logger.php
+++ backend/logger.php
@@ -1,34 +1,22 @@
 <?php
-// includes/logger.php
+
+enum LogLevel {
+    case INFO;
+    case WARN;
+    case ERROR;
+}
 
 class Logger
 {
-    private static $logFile = __DIR__ . '/../logs/app.log';
+    private static $logFile = __DIR__ . 'logs/backend.log';
 
-    public static function log($level, $message)
-    {
+    public static function log(LogLevel $level, string $message) : void {
         if (!file_exists(dirname(self::$logFile))) {
-            mkdir(dirname(self::$logFile), 0777, true);
+            mkdir(dirname(self::$logFile), 0755, true);
         }
 
         $time = date('Y-m-d H:i:s');
-        $entry = "[$time] [$level] $message" . PHP_EOL;
+        $entry = "[$time] [$level->name] $message" . PHP_EOL;
         file_put_contents(self::$logFile, $entry, FILE_APPEND);
     }
-
-    public static function info($message)
-    {
-        self::log('INFO', $message);
-    }
-
-    public static function warning($message)
-    {
-        self::log('WARNING', $message);
-    }
-
-    public static function error($message)
-    {
-        self::log('ERROR', $message);
-    }
 }
-?>
