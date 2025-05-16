883a84f89f1a6df2b65438e1afbdb00bd7ddc51e
paulusel <paulusel@proton.me>
2025-05-16 19:55:16 +0300

    move session based redirection to server side

--- pages/index.php
+++ pages/index.php
@@ -1,3 +1,17 @@
 <?php
 
-header('location: signin.php');
+require_once __DIR__ . '/../backend/helpers.php';
+
+try {
+    $user = idetifyUser(false);
+    if(isset($user['profile'])) {
+        header("Location: home.php");
+    }
+    else {
+        header("Location: profile.php");
+    }
+}
+catch(Exception $e) {
+    header("Location: signin.php");
+}
+?>
--- pages/signin.php
+++ pages/signin.php
@@ -1,3 +1,19 @@
+<?php
+
+require_once __DIR__ . '/../backend/helpers.php';
+
+try {
+    $user = idetifyUser(false);
+    if(isset($user['profile'])) {
+        header("Location: home.php");
+    }
+    else {
+        header("Location: profile.php");
+    }
+}
+catch(Exception $e) {}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
